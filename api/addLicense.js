0abcf7d4cef30db8ce798eef5b6b66a834740713
MerchPS <merchpsbackup@gmail.com>
2025-04-27 17:06:22 +0700

    Update addLicense.js

--- api/addLicense.js
+++ api/addLicense.js
@@ -1,5 +1,9 @@
 const { MongoClient } = require('mongodb');
 
+// Koneksi ke MongoDB Atlas
+const uri = 'mongodb+srv://HansDB:Hansmoses2007#@lisensi.98zue9l.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
+
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
         const { license } = req.body;
@@ -8,22 +12,18 @@ module.exports = async (req, res) => {
             return res.status(400).json({ success: false, message: 'Lisensi tidak boleh kosong' });
         }
 
-        // Koneksi ke MongoDB
-        const uri = 'mongodb+srv://HansDB:Hansmoses2007#@lisensi.98zue9l.mongodb.net/?retryWrites=true&w=majority';
-        const client = new MongoClient(uri);
-
         try {
             // Coba connect ke MongoDB
             await client.connect();
-            const database = client.db('Lisensi');
-            const collection = database.collection('Licenses');
+            const database = client.db('Lisensi');  // Nama database
+            const collection = database.collection('Licenses');  // Nama collection
 
             // Insert lisensi baru ke MongoDB
             await collection.insertOne({ license, created_at: new Date() });
-            console.log('Lisensi berhasil ditambahkan:', license);  // Log untuk memastikan lisensi berhasil ditambahkan
+            console.log('Lisensi berhasil ditambahkan:', license);
             res.status(200).json({ success: true, message: 'Lisensi berhasil ditambahkan!' });
         } catch (error) {
-            console.error('Error saat menyimpan lisensi:', error);  // Log error untuk debugging
+            console.error('Error saat menyimpan lisensi:', error);
             res.status(500).json({ success: false, message: 'Terjadi kesalahan di server: ' + error.message });
         } finally {
             await client.close();
